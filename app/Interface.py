9c46469addc52de7cd162dadbb46821fb7d456b3
Andrey <k4ty224@mail.ru>
2025-05-10 02:17:59 +0300

    add advance model

--- app/Interface.py
+++ app/Interface.py
@@ -1,8 +1,23 @@
 import streamlit as st
 import requests
+import time
+from datetime import datetime
 
 # login_dialog и reg_dialog - это окошки, вызываемые при
 # нажатии соответствующих кнопок
+
+
+def get_time_period():
+    hour = datetime.now().hour
+    if 5 <= hour < 12:
+        return "morning"
+    elif 12 <= hour < 17:
+        return "noon"
+    elif 17 <= hour < 21:
+        return "evening"
+    else:
+        return "night"
+
 
 @st.dialog("Login with credentials")
 def login_dialog():
@@ -13,24 +28,36 @@ def login_dialog():
         if submit:
             with open("users.txt") as file:
                 for line in file:
-                    file_log,file_pass = line.strip().split(" ")
+                    file_log, file_pass, age = line.strip().split(" ")
                     if login == file_log and password == file_pass:
                         st.success("Logged in successfully")
-                        st.session_state.login =  file_log
+                        st.session_state.login = file_log
                         st.session_state.password = file_pass
+                        st.session_state.age = int(age)
                         st.rerun()
             st.error("Wrong credentials")
+
 
 @st.dialog("Registration form")
 def reg_dialog():
     with st.form("reg_form"):
         login = st.text_input("Username")
         password = st.text_input("Password")
+        st.write("To improve the accuracy of the analysis, please indicate your age. This will help us better take into account the peculiarities of mood perception depending on age.")
+        age = st.number_input("Age", min_value=1, max_value=110, step=1)
         submit = st.form_submit_button("Submit")
         if submit:
-            with open("users.txt","a") as file:
-                file.write(f"{login} {password}\n")
-                st.rerun()
+            with open("users.txt", "a") as file:
+                file.write(f"{login} {password} {int(age)}\n")
+
+            st.session_state.login = login
+            st.session_state.password = password
+            st.session_state.age = int(age)
+            st.success("Registration successful! You are now logged in.")
+            st.write("You will now be redirected to the main page.")
+            time.sleep(2)
+            st.rerun()
+
 
 st.title("AI Mood Analyzer")
 
@@ -54,32 +81,54 @@ if st.session_state.login is None:
     if reg_btn:
         reg_dialog()
 else:
-    st.sidebar.header(f"Welcome, {st.session_state.login}",divider="rainbow")
+    st.sidebar.header(f"Welcome, {st.session_state.login}", divider="rainbow")
     if st.sidebar.button("Logout"):
         st.session_state.login = None
         st.session_state.password = None
+        st.session_state.age = None
         st.rerun()
 
-    chosen_model = st.sidebar.selectbox("Choose the model", ['chatGPT 14.5','T-800'])
+    st.sidebar.subheader("💡 Choose a model")
+    st.sidebar.write("""
+    **Simple Model**: A basic model that analyzes text sentiment with fewer parameters. It is faster but might be less accurate.
+    
+    **Advanced Model**: This model uses additional features and more advanced algorithms to achieve better accuracy. It may take more time to analyze text but provides more detailed results.
+    """)
+    chosen_model = st.sidebar.selectbox(
+        "Choose the model", ['simple', 'advance'])
+
     st.write("Chosen model:", chosen_model)
+    time_of_day = get_time_period()
 
     text_input = st.text_input("Write text:")
-    if st.button("Send"):
-        if text_input:
-            response = requests.post(
-                "http://localhost:8000/process/", json={"text": text_input}
-            )
-            if response.status_code == 200:
-                data = response.json()
-                st.subheader("Result of analyze:")
-                st.write("Responce: ", data["label"])
 
-                st.subheader("💡 Explanation:")
-                st.write(f"The model determined that the message was categorized as {data['label']} because it contained the words:")
-                for item in data["explanation"]:
-                    word = item["word"]
-                    score = item["score"]
-                    st.write(f"- **{word}** → influence: {score:+}")
-                st.write(f"These words are characteristic of a {data['label']} mood, and they determined the algorithm of the final decision.")
-            else:
-                st.error("Error")
+    if st.button("Send"):
+
+        if text_input:
+            with st.spinner('Analyzing your text...'):
+                try:
+                    response = requests.post(
+                        "http://localhost:8000/process/", json={"text": text_input,
+                                                                "model": chosen_model,
+                                                                "time": time_of_day,
+                                                                "age": st.session_state.age
+                                                                }
+                    )
+                    if response.status_code == 200:
+                        data = response.json()
+                        st.subheader("Result of analyze:")
+                        st.write("Responce: ", data["label"])
+
+                        st.subheader("💡 Explanation:")
+                        st.write(
+                            f"The model determined that the message was categorized as {data['label']} because it contained the words:")
+                        for item in data["explanation"]:
+                            word = item["word"]
+                            score = item["score"]
+                            st.write(f"- **{word}** → influence: {score:+}")
+                        st.write(
+                            f"These words are characteristic of a {data['label']} mood, and they determined the algorithm of the final decision.")
+                    else:
+                        st.error("Error")
+                except Exception as e:
+                    st.error(f"Error: {e}")
--- app/main.py
+++ app/main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi import Request
-from fastapi.middleware.cors import CORSMiddleware
 import pandas as pd
 import numpy as np
 import re
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, pipeline
+import torch
+from captum.attr import IntegratedGradients
+import numpy as np
 from contextlib import asynccontextmanager
 from functools import partial
 from nltk.corpus import stopwords
@@ -19,17 +22,23 @@ from scipy.sparse import hstack
 
 nltk.download('stopwords')
 stop_words = stopwords.words("english")
-    
+
+
 class Item(BaseModel):
     text: str
+    time: str
+    age: int
+    model: str
+
 
 def dataPrepare():
     data = pd.read_csv("dataset/train.csv", encoding="latin1")
     test_data = pd.read_csv("dataset/test.csv", encoding="latin1")
     df = pd.concat([data, test_data])
-    df.drop(columns=['textID', 'selected_text', 'Population -2020', 'Land Area (Km²)', 'Density (P/Km²)', 'Country'], axis=1, inplace=True)
+    df.drop(columns=['textID', 'selected_text', 'Population -2020',
+            'Land Area (Km²)', 'Density (P/Km²)', 'Country'], axis=1, inplace=True)
     df.dropna(inplace=True)
-    
+
     def clean_text(raw_text):
         text = re.sub(re.compile('<.*?>'), '', str(raw_text))
         text = re.sub(r'[^a-zA-Z0-9\s]', '', str(text))
@@ -43,8 +52,9 @@ def dataPrepare():
         return text
 
     df['text'] = df['text'].apply(clean_text)
-    df['text'] = df['text'].apply(lambda x: [item for item in x.split() if item not in stop_words]).apply(lambda x:" ".join(x))
-    
+    df['text'] = df['text'].apply(lambda x: [item for item in x.split(
+    ) if item not in stop_words]).apply(lambda x: " ".join(x))
+
     def remove_urls(text):
         pattern = re.compile(r'\b(?:http|https|www)\S*')
         return pattern.sub(r'', text)
@@ -52,36 +62,44 @@ def dataPrepare():
     df["text"] = df["text"].apply(remove_urls)
     df = df.reset_index(drop=True)
     return df
-    
+
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     df = dataPrepare()
     y = df['sentiment']
     X = df.drop(['sentiment'], axis=1)
-    
+
     encoder = LabelEncoder()
     y = encoder.fit_transform(y)
-    
+
     cv = CountVectorizer()
     X_text = cv.fit_transform(X["text"])
-    
+
     encoder_time = OneHotEncoder(sparse_output=False)
     new_time = encoder_time.fit_transform(df[['Time of Tweet']])
-    
+
     encoder_age = OneHotEncoder(sparse_output=False)
     new_age = encoder_age.fit_transform(df[['Age of User']])
-    
+
     X_combined = hstack([X_text, new_time, new_age])
-    model = LogisticRegression(max_iter=1000, n_jobs=1)
-    model.fit(X_combined, y)
-    
-    app.state.model = model
+    simple_model = LogisticRegression(max_iter=1000, n_jobs=1)
+    simple_model.fit(X_combined, y)
+
+    model = "cardiffnlp/twitter-roberta-base-sentiment"
+    tokenizer = AutoTokenizer.from_pretrained(model)
+    model = AutoModelForSequenceClassification.from_pretrained(model)
+    model.eval()
+    sentiment_pipeline = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
+    app.state.advance_model = model
+    app.state.pipeline = sentiment_pipeline
+    app.state.tokin = tokenizer
+    app.state.simple_model = simple_model
     app.state.vectorizer = cv
     app.state.time_encoder = encoder_time
     app.state.age_encoder = encoder_age
-    
     yield
-    
+
 app = FastAPI(lifespan=lifespan)
 
 
@@ -92,7 +110,8 @@ def map_label(n):
         return "Neutral"
     elif n == 2:
         return "Positive"
-    
+
+
 def age_to_range(age):
     if 0 <= age <= 20:
         return '0-20'
@@ -107,13 +126,16 @@ def age_to_range(age):
     else:
         return '70-100'
 
+
 def text_clean(text):
     text = re.sub(re.compile('<.*?>'), '', str(text))
     text = re.sub(r'[^a-zA-Z0-9\s]', '', text)
     text = re.sub(r'\s+', ' ', text).strip()
     text = text.lower()
-    clean_text = " ".join([word for word in text.split() if word not in stop_words])
+    clean_text = " ".join(
+        [word for word in text.split() if word not in stop_words])
     return clean_text
+
 
 def lime_predict_proba(texts, time_matrix, age_matrix, request: Request):
     features = []
@@ -125,49 +147,107 @@ def lime_predict_proba(texts, time_matrix, age_matrix, request: Request):
         features.append(t_feature)
     feature_matrix = np.vstack([f.toarray() for f in features])
     feature_matrix = csr_matrix(feature_matrix)
-    model = request.app.state.model
+    model = request.app.state.simple_model
     return model.predict_proba(feature_matrix)
-    
 
 
 @app.post("/process/")
 def process_text(item: Item, request: Request):
-    
-    text = text_clean(item.text)
-    time = "morning"
-    age = 20
-    
-    cv = request.app.state.vectorizer
-    text_vector = cv.transform([text])
-    
-    encoder_time = request.app.state.time_encoder
-    encoder_age = request.app.state.age_encoder
-    
-    time_df = pd.DataFrame([[time]], columns=["Time of Tweet"])
-    time_matrix = encoder_time.transform(time_df)
-    
-    age_range = age_to_range(age)
-    age_df = pd.DataFrame([[age_range]], columns=["Age of User"])
-    age_matrix = encoder_age.transform(age_df)
+    if item.model == "simple":
+        text = text_clean(item.text)
+        time = item.time
+        age = item.age
+
+        cv = request.app.state.vectorizer
+        text_vector = cv.transform([text])
+
+        encoder_time = request.app.state.time_encoder
+        encoder_age = request.app.state.age_encoder
+
+        time_df = pd.DataFrame([[time]], columns=["Time of Tweet"])
+        time_matrix = encoder_time.transform(time_df)
+
+        age_range = age_to_range(age)
+        age_df = pd.DataFrame([[age_range]], columns=["Age of User"])
+        age_matrix = encoder_age.transform(age_df)
+
+        feature = hstack([text_vector, time_matrix, age_matrix])
+
+        model = request.app.state.simple_model
+        pred = model.predict(feature)
+
+        explainer = LimeTextExplainer(
+            class_names=["negative", "neutral", "positive"])
+
+        lime_predict = partial(
+            lime_predict_proba, time_matrix=time_matrix, age_matrix=age_matrix, request=request)
+        explanation = explainer.explain_instance(
+            text, lime_predict, num_features=10)
+
+        top_important_words = explanation.as_list()[:3]
+        explanation_words = [
+            {"word": word, "score": round(score, 3)}
+            for word, score in top_important_words
+        ]
+
+        return {
+            "label": map_label(pred),
+            "explanation": explanation_words
+        }
+    else:
         
-    feature = hstack([text_vector, time_matrix, age_matrix]) 
+        label_mapping = {
+            "LABEL_0": "negative",
+            "LABEL_1": "neutral",
+            "LABEL_2": "positive"
+        }
 
-    model = request.app.state.model
-    pred = model.predict(feature)
+        text = item.text
+        inputs = request.app.state.tokin(text, return_tensors="pt", padding=True, truncation=True)
+        input_ids = inputs['input_ids'].long()  
+        attention_mask = inputs['attention_mask']
+        
+        result = request.app.state.pipeline(text)
+        label = result[0]['label']
+        mapped_label = label_mapping[label]
+        confidence = result[0]['score']
+        label_to_index = {"LABEL_0": 0, "LABEL_1": 1, "LABEL_2": 2}
+        target = label_to_index[label]
 
-    explainer = LimeTextExplainer(class_names=["negative", "neutral", "positive"])
-    
-    lime_predict = partial(lime_predict_proba, time_matrix=time_matrix, age_matrix=age_matrix, request=request)
-    explanation = explainer.explain_instance(text, lime_predict, num_features=10)
-    
-    
-    top_important_words = explanation.as_list()[:3]
-    explanation_words = [
-    {"word": word, "score": round(score, 3)}
-    for word, score in top_important_words
-]
-    
-    return {
-    "label": map_label(pred),
-    "explanation": explanation_words
-}
+        embedding_layer = request.app.state.advance_model.roberta.embeddings  
+        with torch.no_grad():
+            embeddings = embedding_layer(input_ids).clone().detach().requires_grad_(True)
+
+        def forward_func(embeddings, attention_mask):
+            model_inputs = {
+                'inputs_embeds': embeddings,
+                'attention_mask': attention_mask
+            }
+            outputs = request.app.state.advance_model(**model_inputs)
+            return outputs.logits
+
+        ig = IntegratedGradients(forward_func)
+
+        attributions, delta = ig.attribute(
+            inputs=embeddings,
+            additional_forward_args=(attention_mask,),
+            target=target,
+            return_convergence_delta=True
+        )
+
+        attributions_sum = attributions.sum(dim=-1).squeeze(0)
+        attributions_sum = attributions_sum.detach().numpy()
+        tokens = request.app.state.tokin.convert_ids_to_tokens(input_ids.squeeze(0))
+        tokens = [token.replace("Ġ", "") for token in tokens]
+
+        attributions_sum = attributions_sum / np.max(np.abs(attributions_sum))
+        top_indices = np.argsort(np.abs(attributions_sum))[-3:]  
+        explanation_words = []
+        for idx in top_indices:
+            score = float(attributions_sum[idx])  
+            explanation_words.append({"word": tokens[idx], "score": round(score, 3)})
+
+        return {
+            "label": mapped_label,
+            "explanation": explanation_words
+        }
